fd06a8099a7fbf2aee487a57de8949856908a104
Jessica <jssclgh@gmail.com>
2023-12-13 18:40:37 -0600

    added new code to javascript and html

--- assets/script.js
+++ assets/script.js
@@ -1,30 +1,107 @@
 // timer
 var timeEl = document.querySelector(".timer");
 var secondsLeft = 75;
+var pointsPenalty = 10;
+var questionIndex = 0;
 
 // questions + answers arrays - correct answers marked somehow
 
-var myQuestions
-
-//timer function
-
-function setTimer() {
-      // Sets interval in variable
-  var timerInterval = setInterval(function() {
-    secondsLeft--;
-    timeEl.textContent = secondsLeft
-
-    if(secondsLeft === 0) {
-      // Stops execution of action at set interval
-      clearInterval(timerInterval);
+var myQuestions = [
+    {
+        questionText: "what is a?",
+        answers: [
+            "a",
+            "b",
+            "c"
+        ],
+        correctIndex: 1,
+    },
+    {
+        questionText: "how do you?",
+        answers: [
+            "a",
+            "b",
+            "c"
+        ],
+        correctIndex: 1,
+    },
+    {
+        questionText: "?",
+        answers: [
+            "a",
+            "b",
+            "c"
+        ],
+        correctIndex: 1,
+    },
+    {
+        questionText: "?",
+        answers: [
+            "a",
+            "b",
+            "c"
+        ],
+        correctIndex: 1,
+    },
+    {
+        questionText: "?",
+        answers: [
+            "a",
+            "b",
+            "c"
+        ],
+        correctIndex: 1,
+    },
+    {
+        questionText: "?",
+        answers: [
+            "a",
+            "b",
+            "c"
+        ],
+        correctIndex: 1,
+    },
+    {
+        questionText: "?",
+        answers: [
+            "a",
+            "b",
+            "c"
+        ],
+        correctIndex: 1,
+    },
+    {
+        questionText: "?",
+        answers: [
+            "a",
+            "b",
+            "c"
+        ],
+        correctIndex: 1,
     }
+]
 
-  }, 1000);
-}
+var startButton = document.querySelector("startQuiz")
+
 
 //quiz function - subtract time for wrong answers
+function displayTimeLeft() {
+    secondsLeft--;
+    timeLeft.textContent = secondsLeft
+
+}
+
+function displayNextQuestion() {
+    questionText = document.querySelector("#questionText")
+
+}
+
+function startQuiz() {
+    setTimeout(function {
+        displayTimeLeft();   
+    },
+    displayNextQuestion();
+     1000)
+};
 
 // set high scores to save in local storage
-setTimer();
-
-
